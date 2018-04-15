e775054923c2946e49d4f0fd6c022d70a2580001
Steve Oney <swloney@gmail.com>
2018-04-15 10:01:53 -0400

    bugfix for observers

--- built/cc_channel.js
+++ built/cc_channel.js
@@ -24,6 +24,32 @@ class ChatCodesChannelServer extends events_1.EventEmitter {
         if (!this.isArchive()) {
             this.addEditorListeners();
         }
+        this.wss.on('connection', (ws) => {
+            ws.on('message', (str) => {
+                try {
+                    const data = JSON.parse(str);
+                    const { ns } = data;
+                    if (data.cc === 1 && ns === this.getShareDBNamespace()) {
+                        const { type } = data;
+                        if (type === 'get-editors-values') {
+                            const { payload, messageID } = data;
+                            const version = payload;
+                            this.getEditorValues(version).then((result) => {
+                                ws.send(JSON.stringify({
+                                    messageID,
+                                    ns,
+                                    cc: 2,
+                                    payload: Array.from(result.values())
+                                }));
+                            });
+                        }
+                    }
+                }
+                catch (e) {
+                    console.error(e);
+                }
+            });
+        });
     }
     isArchive() {
         return this.archived;
